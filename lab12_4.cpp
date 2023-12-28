9b78d343df5762af6d45e8f284950af6ca570944
temty180447 <tgtv2547@gmail.com>
2023-12-28 22:09:31 +0700

    XD

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template <typename T>
+void mySwap(T &a, T &b);
+
 int main(){
 	int x, y;
 	string a, b;
@@ -28,5 +31,12 @@ int main(){
 	cout << "p = " << p << ", q = " << q << "\n";
 
 	return 0;
+}
+
+template <typename T>
+void mySwap(T &a, T &b) {
+    T temp = a;
+    a = b;
+    b = temp;
 }
 
